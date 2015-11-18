c7bd81eeb134e34f3b2075a2e2d33a8a2f7eaa4e
Peter Kamb <peterkamb@gmail.com>
2015-11-17 16:42:49 -0800

    Add Script controller

---
+++ PowerKey/PKScriptController.h
@@ -0,0 +1,13 @@
+//
+//  PKScriptController.h
+//  PowerKey
+//
+//  Created by Peter Kamb on 11/17/15.
+//  Copyright © 2015 Peter Kamb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PKScriptController : NSObject
+
+@end
---
+++ PowerKey/PKScriptController.m
@@ -0,0 +1,13 @@
+//
+//  PKScriptController.m
+//  PowerKey
+//
+//  Created by Peter Kamb on 11/17/15.
+//  Copyright © 2015 Peter Kamb. All rights reserved.
+//
+
+#import "PKScriptController.h"
+
+@implementation PKScriptController
+
+@end

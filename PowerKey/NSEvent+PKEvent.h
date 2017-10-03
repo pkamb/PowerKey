eb92060ed06f40fbf81743d7879da6f4ced2c73d
Peter Kamb <peterkamb@gmail.com>
2017-10-02 21:42:37 -0700

    add NSEvent category

---
+++ PowerKey/NSEvent+PKEvent.h
@@ -0,0 +1,13 @@
+//
+//  NSEvent+PKEvent.h
+//  PowerKey
+//
+//  Created by Peter Kamb on 10/2/17.
+//  Copyright © 2017 Peter Kamb. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSEvent (PKEvent)
+
+@end
---
+++ PowerKey/NSEvent+PKEvent.m
@@ -0,0 +1,13 @@
+//
+//  NSEvent+PKEvent.m
+//  PowerKey
+//
+//  Created by Peter Kamb on 10/2/17.
+//  Copyright © 2017 Peter Kamb. All rights reserved.
+//
+
+#import "NSEvent+PKEvent.h"
+
+@implementation NSEvent (PKEvent)
+
+@end
